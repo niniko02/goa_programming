421d1bde8bcf381f191418fb08d9434ca65b64da
niniko02 <146019044+niniko02@users.noreply.github.com>
2023-10-05 21:59:59 +0400

    Add files via upload

---
+++ programireba/day 4/homework/user1.py
@@ -0,0 +1,30 @@
+user_name = input("whats your name: ")
+print("as user said, he is ", user_name)
+
+user_lastname = input("whats your last name: ")
+print("as user said, he is ", user_lastname)
+
+user_age = int(input("whats your age: "))
+print("as user said, he is ", user_age)
+
+user_momsname = input("whats your moms name: ")
+print("as user said, he is ", user_momsname)
+
+user_dadsname = input("whats your dads name:  ")
+print("as user said, he is ", user_dadsname)
+
+user_petsname = input("whats your pets name: ")
+print("as user said, he is ", user_petsname)
+
+
+user_15 = input("how old will you be after 15 years: ")
+print("as user said, he is ", user_15)
+
+
+
+
+
+
+
+
+
